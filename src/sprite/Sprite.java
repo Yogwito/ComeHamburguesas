27e85e56053f24d0146b7d362fe3edcba9ef3f06
Juan José Trujillo <juanj.trujillo@autonoma.edu.co>
2025-05-18 22:54:22 -0500

    Actualizacion de la clase Sprite

--- src/sprite/Sprite.java
+++ src/sprite/Sprite.java
@@ -3,16 +3,34 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package sprite;
+
 import java.awt.Graphics;
 
 /**
- *
- * @author juans
+ * 
+ * @author Juans & Trujirendjj
  */
-
 public abstract class Sprite {
-    protected int x, y, width, height;
+    /** Posición horizontal del sprite. */
+    protected int x;
+    
+    /** Posición vertical del sprite. */
+    protected int y;
+    
+    /** Ancho del sprite. */
+    protected int width;
+    
+    /** Alto del sprite. */
+    protected int height;
 
+    /**
+     * Constructor que inicializa la posición y tamaño del sprite.
+     * 
+     * @param x      posición X inicial.
+     * @param y      posición Y inicial.
+     * @param width  ancho del sprite.
+     * @param height alto del sprite.
+     */
     public Sprite(int x, int y, int width, int height) {
         this.x = x;
         this.y = y;
@@ -20,12 +38,28 @@ public abstract class Sprite {
         this.height = height;
     }
     
+    /** @return la posición X del sprite. */
     public int getX() { return x; }
+    
+    /** @return la posición Y del sprite. */
     public int getY() { return y; }
+    
+    /** @return el ancho del sprite. */
     public int getWidth() { return width; }
+    
+    /** @return el alto del sprite. */
     public int getHeight() { return height; }
 
+    /**
+     * Método abstracto para verificar colisiones con otros elementos del juego.
+     * Las subclases deben implementar la lógica específica.
+     */
     public abstract void checkCollision();
+
+    /**
+     * Método abstracto para dibujar el sprite en el contexto gráfico proporcionado.
+     * 
+     * @param g el contexto gráfico donde se realizará el dibujo.
+     */
     public abstract void paint(Graphics g);
 }
-
