c02aaaa53e5bf0d3ebc557d02fc81509b5d44275
Juan José Trujillo <juanj.trujillo@autonoma.edu.co>
2025-05-18 22:56:29 -0500

    Actualizacion de la clase SpriteMobile

--- src/sprite/SpriteMobile.java
+++ src/sprite/SpriteMobile.java
@@ -5,17 +5,28 @@
 package sprite;
 
 /**
- *
- * @author juans
+ * @author Juans & Trujirendjj
  */
 public abstract class SpriteMobile extends Sprite {
 
+    /**
+     * Constructor que inicializa la posición y tamaño del sprite móvil.
+     * 
+     * @param x      posición X inicial.
+     * @param y      posición Y inicial.
+     * @param width  ancho del sprite.
+     * @param height alto del sprite.
+     */
     public SpriteMobile(int x, int y, int width, int height) {
         super(x, y, width, height);
     }
     
+    /**
+     * Mueve el sprite hacia abajo incrementando su coordenada Y según la velocidad dada.
+     * 
+     * @param velocidad la cantidad de píxeles que se mueve hacia abajo.
+     */
     public void moverHaciaAbajo(int velocidad) {
-    this.y += velocidad;
+        this.y += velocidad;
     }
-
 }
