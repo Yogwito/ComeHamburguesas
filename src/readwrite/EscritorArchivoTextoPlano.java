7ef10ca32aeb060b2234a3fe4367384c342862b8
Juan José Trujillo <juanj.trujillo@autonoma.edu.co>
2025-05-18 22:46:58 -0500

    Actualizacion de la clase EscritorArchivoTextoPlano

--- src/readwrite/EscritorArchivoTextoPlano.java
+++ src/readwrite/EscritorArchivoTextoPlano.java
@@ -5,11 +5,19 @@
 package readwrite;
 
 /**
- *
- * @author juans
+ * 
+ * @author Juans & Trujirendjj
  */
-
 public class EscritorArchivoTextoPlano implements Escritor {
+
+    /**
+     * Escribe el contenido especificado en el archivo indicado por la ruta.
+     * 
+     * Este método aún no ha sido implementado.
+     *
+     * @param ruta      la ruta del archivo donde se desea escribir.
+     * @param contenido el contenido de texto a escribir en el archivo.
+     */
     @Override
     public void escribirArchivo(String ruta, String contenido) {
         // TODO: implementar escritura en archivo
